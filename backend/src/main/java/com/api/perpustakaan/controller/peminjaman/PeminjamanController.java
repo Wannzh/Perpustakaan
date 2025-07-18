02bcfe8de6042b2ca16640c4b1f42a7391752e5a
Muhamad Alwan <alwanfdhlrhmn@gmail.com>
2025-07-19 03:28:42 +0700

    fix peminjaman controller

--- backend/src/main/java/com/api/perpustakaan/controller/peminjaman/PeminjamanController.java
+++ backend/src/main/java/com/api/perpustakaan/controller/peminjaman/PeminjamanController.java
@@ -2,11 +2,13 @@ package com.api.perpustakaan.controller.peminjaman;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.perpustakaan.dto.peminjaman.PeminjamanRequestDTO;
+import com.api.perpustakaan.dto.peminjaman.PeminjamanRequestSelfDTO;
 import com.api.perpustakaan.dto.peminjaman.PeminjamanResponseDTO;
 import com.api.perpustakaan.service.peminjaman.PeminjamanService;
 
@@ -14,14 +16,22 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/peminjaman/manual")
+@RequestMapping("/api/peminjaman")
 @RequiredArgsConstructor
-@Tag(name = "Peminjaman Manual", description = "Endpoints untuk mengelola peminjaman yang bisa di akses oleh pustakawan")
+@Tag(name = "Peminjaman", description = "Endpoints untuk mengelola peminjaman yang bisa di akses oleh pustakawan dan siswa")
 public class PeminjamanController {
     private final PeminjamanService peminjamanService;
 
-    @PostMapping("/tambah")
+    @PostMapping("/manual/tambah")
     public ResponseEntity<PeminjamanResponseDTO> createManual(@RequestBody PeminjamanRequestDTO request) {
         return ResponseEntity.ok(peminjamanService.createManual(request));
+    }
+
+    @PostMapping("/self/tambah")
+    public ResponseEntity<PeminjamanResponseDTO> pinjamMandiri(
+            @RequestBody PeminjamanRequestSelfDTO request,
+            @RequestAttribute("username") String username
+    ) {
+        return ResponseEntity.ok(peminjamanService.createSelfPeminjaman(username, request));
     }
 }
